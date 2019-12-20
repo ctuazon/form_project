476a25ec50fd96c374c2c462cafef94838806c08
ctuazon <58759231+ctuazon@users.noreply.github.com>
2019-12-19 21:14:37 -0500

    Add files via upload

--- outputdata.php
+++ outputdata.php
@@ -10,17 +10,90 @@ foreach (SelectBlog() as $output)
 		if ($key == 'id')
 		{	
 			$id = $out;
+			//echo "<div id='".$id."'>".$id."</div>";
 		}
 		else
-		{	
+		{
+			switch ($key)
+			{
+				case "name":
+					$key = "Name";
+					break;
+				case "email":
+					$key = "Email";
+					break;
+				case  "numcards":
+					$key = "Number Of Cards";
+					break;
+				case  "typecards":
+					$key = "Type Of Cards";
+					break;
+				case  "rewardsgood":
+					$key = "Are Rewards Good?";
+					break;
+				case  "oftencards":
+					$key = "Do you use it Online?";
+					break;
+				case  "comments":
+					$key = "Comments";
+					break;
+				default:
+					$key = "error";
+				
+			}
+			
 			echo "<div id='values'>";
 				echo "<div id='left'>". $key ."</div><div id='right'> ".$out . "</div>";
 			echo "</div>";
 		}
 	}
-	echo "<a href ='delete.php?id=".$id."'>Delete</a><br/>";
+		echo "<div id ='deletetrigger'>";
+			echo 	'<a href="#" class="winter" id="'.$id.'">Delete</a>';
+		echo "</div>";
 	echo "</div>";
 }
 
 
 ?>
+
+<script>
+$(document).ready(function() 
+{
+	// delete user
+	$('.winter').click(function(event)
+	{
+		event.preventDefault();
+	
+		var process = confirm("Do you wish to delete this user?");
+	
+		var id = $(this).attr('id');
+	
+		//if(id == '1')
+		if (process)
+		{
+		
+			
+			$.ajax({
+				type: 'post',
+				url: 'deleteuser.php',
+			
+				data: {
+					id1:id
+				},
+				
+				success: function (response) 
+				{
+				
+					console.log("USER DEAD");
+				}
+			
+				
+			});
+		}
+		else
+		{
+			//redirect
+		}
+	});
+});
+</script>
